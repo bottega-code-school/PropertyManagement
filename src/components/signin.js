33ff4c183a88af57e5eb0d429601761e2552a55e
Max <maxnelson997@gmail.com>
2018-04-12 17:13:24 -0600

    setup auth routes

---
+++ src/components/signin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Signin extends Component {
+    render() {
+        return (
+            <div>
+                Signin form 
+            </div>
+        )
+    }
+}
+
+export default Signin;
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Signin from './components/signin';
+import Signup from './components/signup';
+
 class Routes extends Component {
     render() {
         return (
             <div>
-                this is our routes
+                <header>
+                    <h1>welcome to the HOA manager</h1>
+                    <h2>please login to continue</h2>
+                </header>
+                <Switch>
+                    <Route path="/signIn" component={Signin}></Route>
+                    <Route path="/signUp" component={Signup}></Route>
+                </Switch>
             </div>
         )
     }
