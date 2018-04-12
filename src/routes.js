9af411b2c36bb299297d5a2717fdadec7961b6bf
Max <maxnelson997@gmail.com>
2018-04-12 16:16:36 -0600

    setup routes component

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+class Routes extends Component {
+    render() {
+        return (
+            <div>
+                this is our routes
+            </div>
+        )
+    }
+}
+
+export default Routes;
